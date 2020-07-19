adbc74ea0a8e7690f985f19f1acf75677e25134b
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-07-19 03:13:35 -0300

    Criado passo a passo do funcionamento

---
+++ src/components/HomeSteps.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="text-center my-6">
+    <h1>Faça parte da Help.in</h1>
+    <div class="col-sm-8 mx-auto">
+      <v-row justify="space-around" align="center">
+        <v-col cols="auto" sm="4">
+          <v-card max-width="100%" color="transparent" flat class=" text-center pa-5">
+            <v-img src="../assets/step1.png"></v-img>
+            <h3>Cadastro</h3>
+            <v-card-text class="text=justify">
+              Em "Seja membro" você envia suas informações e assina nosso
+              contrato digital.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="auto" sm="4">
+          <v-card max-width="100%" color="transparent" flat class=" text-center pa-5">
+            <v-img src="../assets/step2.png"></v-img>
+            <h3>Cadastre seus serviços</h3>
+            <v-card-text class="text=justify">
+              Disponibilize seus projetos e ajude pessoas de todos os lugares.
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="auto" sm="4">
+           <v-card max-width="100%" color="transparent" flat class=" text-center pa-5">
+            <v-img src="../assets/step3.png"></v-img>
+            <h3>Conecte pessoas</h3>
+            <v-card-text class="text=justify">
+              Conecte pessoas do mundo todo através da sua ong em prol da humanidade
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
