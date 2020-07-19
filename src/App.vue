f9d1d16c1307976d97e57760c364ddbf64208eb2
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-07-19 03:10:51 -0300

    Criado app do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app id="app">
+    <AdmToolBar />
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+    <v-footer></v-footer>
+  </v-app>
 </template>
+
+<script>
+import AdmToolBar from "@/components/AdmToolBar.vue";
+export default {
+name: "App",
+components: {
+  AdmToolBar,
+}
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: Montserrat !important;
+  background-color: #f7f8fa;
 }
-
-#nav {
-  padding: 30px;
+.v-btn {
+  text-transform: none !important;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+::-webkit-scrollbar {
+  display: none;
 }
 </style>
