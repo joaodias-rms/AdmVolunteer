cd13bb40b2d30cd20e8c840ab1ce62e08985f477
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-07-19 04:36:25 -0300

    Criada view de registro

---
+++ src/views/Register.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-center">
+      <Register />
+    <router-view> </router-view>
+  </div>
+</template>
+
+<script>
+import Register from "@/components/Register.vue";
+export default {
+  components: {
+    Register
+  },
+};
+</script>
+
+<style></style>
