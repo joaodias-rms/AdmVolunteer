5fdaa84880dd0d4d91bad93e48d09aa1d101715f
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-07-19 03:12:43 -0300

    Criado toolbar

---
+++ src/components/AdmToolBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app-bar color="#8634eb" class="white--text" right flat max-height="50px">
+    <v-btn
+      to="/"
+      color="transparent"
+      width="120"
+      text
+      class="justify-center"
+      depressed
+      small
+    >
+      <Icon Width="120" Height="25px" colorIcon="white"
+    /></v-btn>
+    <v-spacer></v-spacer>
+    <v-btn class="mr-3" text color="white">Já sou parceiro</v-btn>
+    <v-btn class="mr-3" text color="white">Ajuda</v-btn>
+    <v-btn class="mr-3" text color="white">Medidas COVID-19</v-btn>
+    <v-btn class="mr-3" outlined color="white">Faça parte</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+import Icon from "@/components/Icon.vue";
+export default {
+  components: {
+    Icon,
+  }
+};
+</script>
+
+<style>
+</style>
