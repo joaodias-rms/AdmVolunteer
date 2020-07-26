d1101277617565052b75413ac9fb9dc7e2b4d3c7
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-07-26 02:33:10 -0300

    Criada view da tela de login

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+      <v-img  height="600" width="60" src="../assets/ongs.jpg"></v-img>
+    <v-col sm="6">
+      <LoginScreen />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import LoginScreen from "@/components/LoginScreen.vue";
+export default {
+  components: {
+    LoginScreen,
+  }
+};
+</script>
+
+<style>
+</style>
