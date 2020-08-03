5997e6781e7cfb6413cde818d6c477b26ec360f1
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-08-03 20:40:26 -0300

    Criada view da tela inicial

---
+++ src/views/ProfileHome.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <v-row>
+      <v-col>
+        <SideMenu />
+      </v-col>
+      <v-col>
+        <Projects />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import SideMenu from "@/components/SideMenu.vue";
+import Projects from "@/components/Projects.vue";
+export default {
+  components: {
+    SideMenu,
+    Projects
+  }
+};
+</script>
+
+<style>
+</style>
