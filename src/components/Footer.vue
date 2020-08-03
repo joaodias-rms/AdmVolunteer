2a7362d1573cef8b209efe8d0aaaa0b8887de568
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-08-03 20:39:57 -0300

    Adicionado novas informações ao footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,15 @@
 <template>
   <v-card tile class="text-center" color="#8634eb" flat width="100%">
     <v-card-text>
-      <Icon Width="100%" Height="30px" colorIcon="#FFF" />
+      <Icon width="100%" height="30px" colorIcon="#FFF" />
       <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
         <v-icon size="24px">{{ icon }}</v-icon>
       </v-btn>
     </v-card-text>
-    <v-card-text> </v-card-text>
+
     <v-card-text class="white--text">
-      {{ new Date().getFullYear() }} — <strong>by Jão</strong>
+      {{ new Date().getFullYear() }} —
+      <strong>by Jão - Todos os direitos reservados</strong>
     </v-card-text>
   </v-card>
 </template>
@@ -17,11 +18,17 @@
 import Icon from "@/components/Icon.vue";
 export default {
   components: {
-    Icon,
+    Icon
   },
   data: () => ({
-    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
-  }),
+    icons: [
+      "mdi-facebook",
+      "mdi-twitter",
+      "mdi-linkedin",
+      "mdi-instagram",
+      "mdi-github"
+    ]
+  })
 };
 </script>
 
