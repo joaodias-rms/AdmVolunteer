f5a88b5f1a8660124385cfdfde99cf52ef705b02
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-08-09 22:04:41 -0300

    Criada view de gerenciamento de sessão

---
+++ src/views/Session.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row no-gutters>
+    <v-col sm="6">
+      <v-img height="600" width="100%" src="../assets/ongs.jpg"></v-img>
+    </v-col>
+    <v-col sm="6">
+      <router-view></router-view>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {},
+};
+</script>
+
+<style></style>
