a2b9034b0fb1f252bc23dfac22398c44c06e4cf4
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-08-09 22:02:57 -0300

    criado componente de detalhes da onf

---
+++ src/components/Session/OngDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-sm-12">
+    <b><span>Selecione a logo da sua ONG</span></b>
+    <v-image-input
+      :uploadIcon="'mdi-upload'"
+      v-model="imageData"
+      :image-quality="0.85"
+      clearable
+      image-format="jpeg"
+    />
+  </div>
+</template>
+
+<script>
+import VImageInput from "vuetify-image-input";
+export default {
+  components: {
+    [VImageInput.name]: VImageInput,
+  },
+};
+</script>
+
+<style></style>
